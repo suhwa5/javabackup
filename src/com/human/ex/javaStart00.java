ad75b96fa12a96a76d78c868213e1d4751814cd5
hu-17 <hu-17@hu-17-PC>
2020-02-21 15:57:41 +0900

    20200221backjava2

---
+++ src/com/human/ex/javaStart00.java
@@ -0,0 +1,8 @@
+package com.human.ex;
+public class javaStart00 {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("ÈŞ¸Õ±³À°¼¾ÅÍ");   
+		System.out.println("ÃÖÇöÀç");   
+	}
+}
---
+++ src/com/human/ex1/javaStart00.java
@@ -0,0 +1,6 @@
+package com.human.ex1;
+public class javaStart00 {
+	public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+	}
+}
